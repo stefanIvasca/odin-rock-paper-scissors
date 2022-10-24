cd638fdcf2f3e177d967c86fad8b55f41abe15a1
stefanIvasca <stefanivasca@gmail.com>
2022-10-24 13:42:47 +0300

    play one game at a time and console.log the result of each game

--- myscript.js
+++ myscript.js
@@ -1,13 +1,43 @@
 
 //get random between those 3 options
+const rock='rock';
+const paper='paper';
+const scissors='scissors';
+
 function getComputerChoice(){
     const randomNumber=Math.random()*10;
     if (randomNumber<3.33){
-        return 'rock';
+        return rock;
     }else if (randomNumber<6.66){
-        return 'paper';
+        return paper;
     } else {
-        return 'scissors';
+        return scissors;
     }
 }
 
+const playerSelection=prompt('Enter selection').toLocaleLowerCase();
+const computerSelection=getComputerChoice();
+
+
+//all combination for win and lose
+function playRound(playerSelection, computerSelection){
+    if (playerSelection==computerSelection){
+        console.log('draw');
+    }else if(playerSelection==rock&&computerSelection==paper){
+        console.log('You lose');
+    }else if(playerSelection==rock&&computerSelection==scissors){
+            console.log('You win');
+    }else if(playerSelection==paper&&computerSelection==scissors){
+        console.log('You lose');
+    }else if(playerSelection==paper&&computerSelection==rock){
+        console.log('You win');
+    }else if (playerSelection==scissors&&computerSelection==rock){
+        console.log('You lose');
+    }else if(playerSelection==scissors&&computerSelection==paper){
+        console.log('You win');
+    }else{
+        console.log('insert the right word')
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));
